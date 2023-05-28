6e56cf08951106e826403b4a3bbb3c90d2ceb708
alexdoffgit <noreply@plea.se>
2023-05-28 13:56:21 +0700

    add published column in article table migration

--- backend/db/migrations/20230527032215_create_articles_table.sql
+++ backend/db/migrations/20230527032215_create_articles_table.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS conduit_articles (
     body TEXT NOT NULL,
     slug TEXT NOT NULL UNIQUE,
     author_id TEXT,
+    published BOOLEAN DEFAULT false,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     CONSTRAINT fk_conduit_articles_conduit_users FOREIGN KEY(author_id) REFERENCES conduit_users(id)
