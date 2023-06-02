024fc069b0a86c0a37ab09422916612ea8b26ffe
alexdoffgit <noreply@plea.se>
2023-06-02 13:49:29 +0700

    remove common monorepo

--- common/registration.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const RegisterBody = z.object({
-    user: z.object({
-        username: z.string(),
-        email: z.string().email(),
-        password: z.string()
-    })
-})
